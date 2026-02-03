d5247bbcf23e867082e3f9ea062077de43d66fc5
re-saishun <zakaichi.main@gmail.com>
2026-02-03 13:11:33 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,55 +1,20 @@
-const CACHE_NAME = 'belajarjepun-v2';
-const urlsToCache = [
+const CACHE_NAME = 'belajarjepun-v1';
+const assets = [
   '/',
   '/index.html',
-  '/manifest.json',
+  '/css/style.css',
+  '/js/app.js',
+  '/js/pwa-setup.js',
+  '/data/materi.json', // File materi disimpan di cache!
+  '/manifest.json'
 ];
 
-// Install event - skip waiting to activate immediately
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-// Fetch event - network first, then cache
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-          return response;
-        }
-        const responseToCache = response.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseToCache);
-        });
-        return response;
-      })
-      .catch(() => {
-        return caches.match(event.request);
-      })
-  );
-});
-
-// Activate event - clear all old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => {
-      return self.clients.claim();
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
